5467b2100f75b0ff7bee405d8cddc4947cc4bc88
StephensSTEM <loneilteaches@gmail.com>
2025-08-30 19:06:10 -0500

    changed all original bugs and implicit conversions to explicit conversions, added in my own example of an implicit conversion and an example of explicit conversion using the boolean falsy null

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -18,15 +18,21 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 */
 
-
-let result = "5" - 2;
+let str = "5";//setting the variable str to a string value of "5" so it is a explicit conversion
+let result = Number(str)- 2;//using Number() to explicitly convert the string "5" to the number 5, allowing for correct arithmetic operation
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let isValid = Boolean(true);//using Boolean() to explicitly convert true to a boolean value, which is still true
 if (isValid) {
     console.log("This is valid!");
 }
 
 let age = "25";
-let totalAge = age + 5;
+let totalAge = Number(age) + 5;//using Number() to explicitly convert the string "25" to the number 25, allowing for correct arithmetic operation
 console.log("Total Age: " + totalAge);
+
+let totalTaxDue = ".0725" * 500
+console.log("Total Tax Due: " + totalTaxDue);//implicit conversion occurs here, as JavaScript automatically converts the string ".0725" to a number for the multiplication operation
+
+let booleanTest = null;
+console.log ("Boolean of null is: " + Boolean(booleanTest));//using Boolean() to explicitly convert null to a boolean value, which results in false
